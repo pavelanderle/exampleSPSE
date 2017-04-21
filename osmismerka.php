97d45212d6a9508a745bb611f1233017b9549eab
pavelanderle <anderle@spseplzen.cz>
2017-04-21 10:31:01 +0200

    initial project version

---
+++ osmismerka.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Osmisměrka</title>
+  </head>
+  <body>
+
+<?php
+
+$osmismerka = array(array('v','a','v','ř','í','n','y'), array('a','ý','k','l','i','s','t'), array('v','o','d','o','v','o','d'),array('ř','e','h','o','l','n','í'),array('í','u','t','b','r','ř','d'),array('n','m','s','t','u','n','n'),array('y','c','v','a','b','l','ě'));
+
+
+/* Zobrazení dvourozměrného pole jako html tabulka */
+function array2ToTable($array2){
+    //todo
+}
+
+
+/* Výpis řádek dvourozměrného pole*/
+function extractRow($array2){
+    //todo
+}
+
+/* Výpis sloupců dvourozměrného pole */
+function extractCol($array2){
+    //todo
+}
+
+
+/* výpis diagonál dvourozměrného pol*/
+function extractDiagonal($array2){
+    //todo
+}
+
+/* funkce vrací řádky dvourozměrného pole v poli */
+function rowsToArray($array2){
+    //todo
+    return rows;
+}
+
+
+/* funkce vrací sloupce dvourozměrného pole v poli */
+function colsToArray($array2){
+    //todo
+    return cols;
+}
+
+/* funkce vrací diagonály dvourozměrného pole v poli */
+function diagonalsToArray($array2){
+    //todo
+    return diagonals;
+}
+
+
+?>
+</body>
+</html>
